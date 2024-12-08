884ea45cea8739658b6baba8e2cf71df226f36f0
rowdy-byte <rowdy.verbeek@outlook.com>
2024-12-08 20:15:16 +0100

    word analytics app init

---
+++ src/Container.jsx
@@ -0,0 +1,11 @@
+import Textarea from "./Textarea";
+import Stats from "./Stats";
+
+export default function Container() {
+  return (
+    <main className="container">
+      <Textarea />
+      <Stats />
+    </main>
+  );
+}
---
+++ src/Textarea.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Textarea() {
+  const [text, setText] = useState("");
+  return (
+    <textarea
+      value={text}
+      onChange={(event) => {
+        const newText = event.target.value;
+        setText(newText);
+      }}
+      className="textarea"
+      placeholder="Enter your text"
+      spellCheck="false"
+    />
+  );
+}
