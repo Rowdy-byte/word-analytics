58443fbf054a96c8c3a28793a064b59e74da25b4
rowdy-byte <rowdy.verbeek@outlook.com>
2024-12-10 12:41:39 +0100

    finished

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,14 +1,18 @@
 import Textarea from "./Textarea";
 import Stats from "./Stats";
 import { useState } from "react";
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constants";
 
 export default function Container() {
   const [text, setText] = useState("");
   const stats = {
     numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
     numberOfCharacters: text.length,
-    instagramCharactersLeft: 280 - text.length,
-    facebookCharactersLeft: 2200 - text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
   };
 
   return (
